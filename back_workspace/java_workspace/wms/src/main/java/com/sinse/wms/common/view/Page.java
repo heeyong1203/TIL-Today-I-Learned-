b14f1816658788f47e5633657525f133ec685587
heeyong1203 <rhyong1203@gmail.com>
2025-06-17 01:41:38 +0900

    class 파일 제거 및 team project 기본UI 세팅 업데이트

--- back_workspace/java_workspace/wms/src/main/java/com/sinse/wms/common/view/Page.java
+++ back_workspace/java_workspace/wms/src/main/java/com/sinse/wms/common/view/Page.java
@@ -9,8 +9,10 @@ import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Flow;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
@@ -33,9 +35,6 @@ public class Page extends JPanel {
 
 	AppMain appMain;
 	ImageUtil imageUtil;
-	
-	private List<JPanel> mainPanels = new ArrayList();
-	private List<JPanel> subPanels = new ArrayList();
 
 	public Page(AppMain appMain) {
 		// 생성
@@ -80,119 +79,142 @@ public class Page extends JPanel {
 
 		appMain.add(p_navi, BorderLayout.WEST);
 		appMain.add(p_section, BorderLayout.CENTER);
-		
+
 	}
 
-	public Box createGlobal() {
-		// 생성
-		Box box = Box.createVerticalBox();
-		JLabel global_img1 = new JLabel(new ImageIcon(imageUtil.getImage("user_info.png", 50, 50)));
-		JLabel global_img2 = new JLabel(new ImageIcon(imageUtil.getImage("message.png", 50, 50)));
-		JLabel global_img3 = new JLabel(new ImageIcon(imageUtil.getImage("search.png", 50, 50)));
-		JLabel global_img4 = new JLabel(new ImageIcon(imageUtil.getImage("help.png", 50, 50)));
-		JLabel global_img5 = new JLabel(new ImageIcon(imageUtil.getImage("settings.png", 50, 50)));
+	public JPanel createGlobal() {
+		// ===== 생성 =====
+		JPanel panel = new JPanel();
+		JPanel empty1 = new JPanel();
+		JPanel empty2 = new JPanel();
+		JPanel[] panels = new JPanel[9];
 
-		// 스타일
-		box.add(box.createVerticalStrut(55));
-		box.add(global_img1);
-		box.add(box.createVerticalStrut(50));
-		box.add(global_img2);
-		box.add(box.createVerticalStrut(50));
-		box.add(global_img3);
-		box.add(box.createVerticalStrut(400));
-		box.add(global_img4);
-		box.add(box.createVerticalStrut(50));
-		box.add(global_img5);
+		JLabel global_img0 = new JLabel(new ImageIcon(imageUtil.getImage("user_info.png", 50, 50)));
+		JLabel global_img1 = new JLabel(new ImageIcon(imageUtil.getImage("message.png", 50, 50)));
+		JLabel global_img2 = new JLabel(new ImageIcon(imageUtil.getImage("search.png", 50, 50)));
+		JLabel global_img7 = new JLabel(new ImageIcon(imageUtil.getImage("help.png", 50, 50)));
+		JLabel global_img8 = new JLabel(new ImageIcon(imageUtil.getImage("settings.png", 50, 50)));
 
-		// 조립
-		return box;
+		// ===== 스타일 =====
+		panel.setPreferredSize(new Dimension(100, 960));
+		panel.setOpaque(false);
+
+		empty1.setPreferredSize(new Dimension(100, 20));
+		empty2.setPreferredSize(new Dimension(100, 20));
+		empty1.setOpaque(false);
+		empty2.setOpaque(false);
+
+		for (int i = 0; i < panels.length; i++) {
+			panels[i] = new JPanel();
+			panels[i].setPreferredSize(new Dimension(90, 90));
+			panels[i].setOpaque(false);
+			panels[i].setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+		}
+
+		// ===== 조립 =====
+		panels[0].add(global_img0);
+		panels[1].add(global_img1);
+		panels[2].add(global_img2);
+		panels[7].add(global_img7);
+		panels[8].add(global_img8);
+
+		panel.add(empty1);
+		for (JPanel p : panels) {
+			panel.add(p);
+		}
+		panel.add(empty2);
+
+		return panel;
 	}
 
 	public JPanel createLocal() {
-		// 생성
-		JPanel p_title = new JPanel();
-		JLabel la_localTitle = new JLabel("관리자 페이지"); // local_navi title
-		// 스타일
-		p_title.setPreferredSize(new Dimension(350, 960));
-		la_localTitle.setPreferredSize(new Dimension(350, 140));
-		la_localTitle.setFont(new Font("inter", Font.BOLD, 45));
-		la_localTitle.setForeground(new Color(38, 124, 181));
-		la_localTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		p_title.setOpaque(false);
-		// 조합
-		p_title.add(la_localTitle);
-		
-		// 로컬 메뉴 생성
+		// ===== 로컬 타이틀 =====
+		// ===== 생성 =====
+		JPanel p_container = new JPanel(); // localPanel
+		JLabel la_title = new JLabel("관리자 페이지");
+		JPanel[] mainPanels = new JPanel[localMenus.length];
+		JPanel[] subPanels = new JPanel[localMenus.length];
+
+		// ===== 스타일 =====
+		p_container.setPreferredSize(new Dimension(349, 960));
+		la_title.setPreferredSize(new Dimension(349, 200));
+		la_title.setFont(new Font("inter", Font.BOLD, 45));
+		la_title.setForeground(new Color(38, 124, 181));
+		la_title.setHorizontalAlignment(SwingConstants.CENTER);
+		la_title.setVerticalAlignment(SwingConstants.CENTER);
+
+		// ===== 조립 =====
+		p_container.add(la_title);
+
+		// ===== 메인 메뉴 =====
 		for (int i = 0; i < localMenus.length; i++) {
-			// 생성
-			JPanel p_container = new JPanel();
-			JPanel p_main = new JPanel();
-			JPanel p_sub = new JPanel();
-			JLabel la_img = new JLabel(new ImageIcon(imageUtil.getImage(localMenus[i][0], 75, 75)));
-			JLabel la_text = new JLabel(localMenus[i][1]);
-			// 스타일
-			p_container.setPreferredSize(new Dimension(348, 80 + (localSubMenus[i].length*30)));
-			p_main.setPreferredSize(new Dimension(348, 80)); 
-			p_sub.setPreferredSize(new Dimension(330, (localSubMenus[i].length*30))); 
-			la_text.setPreferredSize(new Dimension(220, 75));
-			la_text.setFont(new Font("inter", Font.BOLD, 30)); 
-			la_text.setForeground(new Color(138, 138, 138));
-			p_container.setOpaque(false);
-			p_main.setOpaque(true);
-			p_sub.setOpaque(false);
-			//p_sub.setVisible(false);
-			// 조립
-			p_main.add(la_img); 
-			p_main.add(la_text);
-			p_container.add(p_main);
-			p_container.add(p_sub);
-			
-			mainPanels.add(p_main);
-			subPanels.add(p_sub);
-			
-			// 로컬 하위 메뉴 생성
-			if (localSubMenus[i] != null && localSubMenus[i].length > 0) {
-				for (int j = 0; j < localSubMenus[i].length; j++) {
-					// 생성
-					JLabel la_sub = new JLabel(localSubMenus[i][j]);
-					JPanel p_blank = new JPanel();
-					// 스타일
-					p_blank.setPreferredSize(new Dimension(20, 20));					
-					la_sub.setFont(new Font("inter", Font.BOLD, 20));
-					la_sub.setForeground(new Color(138, 138, 138));
-					la_sub.setPreferredSize(new Dimension(300, 20));
-					p_blank.setOpaque(false);
-					// 조립
-					p_sub.add(p_blank);
-					p_sub.add(la_sub);
-				}
-			}
-			
+			// ===== 생성 =====
+			mainPanels[i] = new JPanel();
+			subPanels[i] = new JPanel();
+			JPanel p_img = new JPanel();
+			JLabel img = new JLabel(new ImageIcon(imageUtil.getImage(localMenus[i][0], 75, 75)));
+			JLabel la_main = new JLabel(localMenus[i][1]);
+
+			// ===== 스타일 =====
+			p_img.setPreferredSize(new Dimension(100, 100));
+			p_img.setLayout(new FlowLayout(FlowLayout.CENTER, 12, 12));
+			p_img.setOpaque(false);
+			la_main.setPreferredSize(new Dimension(224, 100));
+			la_main.setFont(new Font("inter", Font.BOLD, 30));
+			la_main.setForeground(new Color(138, 138, 138));
+			mainPanels[i].setLayout(new FlowLayout(FlowLayout.LEFT, 25, 0));
+			mainPanels[i].setBackground(Color.BLACK);
+			mainPanels[i].setOpaque(false);
+
+			// ===== 조립 =====
+			p_img.add(img);
+			mainPanels[i].add(p_img);
+			mainPanels[i].add(la_main);
+			p_container.add(mainPanels[i]);
+
 			// 클릭 이벤트 부착
-			final int index = i; // 익명 클래스 내부에서 사용하기 위한 final index
-			p_main.addMouseListener(new MouseAdapter() {
+			final int index_main = i;
+			mainPanels[index_main].addMouseListener(new MouseAdapter() {
 				public void mouseClicked(MouseEvent e) {
-					// 1. 모든 mainPanel 색상 초기화 및 모든 subPanel 숨김 처리
-					for(int j = 0; j < mainPanels.size(); j++) {
-						JPanel mp = mainPanels.get(j);
-						JPanel sp = subPanels.get(j);
-						mp.setBackground(null);
-						sp.setVisible(false);
+					// 모든 패널의 배경색 초기화
+					for (int j = 0; j < mainPanels.length; j++) {
+						mainPanels[j].setOpaque(false);
 					}
-					// 2. 현재 클릭한 패널만 강조 색상 및 하위 메뉴 보이기
-					p_main.setBackground(new Color(149, 199, 233));
-					p_sub.setVisible(true);
-					p_local.revalidate();
-					p_local.repaint();
-					p_local.updateUI();
+					
+					// 클릭한 패널만 색상 지정
+					mainPanels[index_main].setBackground(new Color(149, 199, 233));
+					mainPanels[index_main].setOpaque(true);
+					p_container.updateUI();
 				}
 			});
 			
-			
-			
-			p_title.add(p_container);
-		}
-		return p_title;
+			// ===== 서브 메뉴 =====
+			if (localSubMenus[i] != null) {
+				for (int j = 0; j < localSubMenus[i].length; j++) {
+					// ===== 생성 =====
+					JPanel p_blank = new JPanel();
+					JPanel p_sub = new JPanel();
+					JLabel la_sub = new JLabel(localSubMenus[i][j]);
+
+					// ===== 스타일 =====
+					p_blank.setPreferredSize(new Dimension(40, 30));
+					p_blank.setOpaque(false);
+					p_sub.setPreferredSize(new Dimension(274,30));
+					p_sub.setOpaque(false);
+					la_sub.setPreferredSize(new Dimension(274, 30));
+					la_sub.setBackground(Color.red);
+					la_sub.setFont(new Font("inter", Font.BOLD, 20));
+					la_sub.setForeground(new Color(138, 138, 138));
+
+					// ===== 조립 =====
+					p_sub.add(la_sub);
+					p_container.add(p_blank);
+					p_container.add(p_sub);
+					
+					final int index_sub = j;
+				}
+			} 
+		} return p_container;
 	}
 
 	// 상단 유저 현황 패널
@@ -222,7 +244,7 @@ public class Page extends JPanel {
 		JPanel p_blank = new JPanel();
 		JPanel p_userIcon = new JPanel();
 		JPanel p_user = new JPanel();
-		JLabel la_blank =new JLabel();
+		JLabel la_blank = new JLabel();
 		JLabel la_userIcon = new JLabel(new ImageIcon(imageUtil.getImage("user_info.png", 40, 40)));
 		JLabel la_user = new JLabel("<html><div style='text-align: center;'>마케팅 1팀<br>김범수 님</div></html>");
 
